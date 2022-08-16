1729fbe74dde3f8e29bd554c4e784d5afe332fb9
Peter Kim <taesun1990@gmail.com>
2022-08-16 18:00:44 -0500

    useContext

--- src/App.js
+++ src/App.js
@@ -3,16 +3,30 @@ import AllPosts from "./containers/AllPosts";
 import SameAuthor from "./containers/SameAuthor";
 import Header from "./components/Header";
 import PostDetails from "./containers/PostDetails";
+import { createContext, useEffect, useState } from 'react';
+import { getPosts } from './utils/fetchData';
+
+export const postsContext = createContext()
 
 function App() {
+  // We are creating "global store" that other contexts can use. 
+  const [posts, setPosts] = useState(null)
+
+  useEffect(() => {
+    getPosts().then((posts) => {
+      setPosts(posts);
+    });
+  }, []);
   return (
     <div className="bg-white border border-slate-800 rounded-md max-h-[90vh] mt-[35px] max-w-[300px] sm:max-w-[500px] md:max-w-[600px] mx-auto overflow-auto flex flex-col">
       <Header />
-      <Routes>
-        <Route path="/" exact element={<AllPosts />} />
-        <Route path="/post/:id" element={<PostDetails />} />
-        <Route path="/post/:id/author/:userId" element={<SameAuthor />} />
-      </Routes>
+      <postsContext.Provider value={{ posts, setPosts }}>
+        <Routes>
+          <Route path="/" exact element={<AllPosts />} />
+          <Route path="/post/:id" element={<PostDetails />} />
+          <Route path="/post/:id/author/:userId" element={<SameAuthor />} />
+        </Routes>
+      </postsContext.Provider>
     </div>
   );
 }
--- src/containers/AllPosts.js
+++ src/containers/AllPosts.js
@@ -1,19 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Post from "../components/Post";
 import { getPosts } from "../utils/fetchData";
 import SubHeading from "../components/SubHeading";
+import { postsContext } from '../App';
 
 function AllPosts() {
-  const [posts, setPosts] = useState(null);
-
-  useEffect(() => {
-    if (!posts) {
-      getPosts().then((posts) => {
-        setPosts(posts);
-      });
-    }
-  }, [posts]);
+  const { posts }  = useContext(postsContext);
 
   if (!posts) return <div>Loading...</div>;
 
